61d59e460416aaa619bf73a4c145e4e1b6a19b68
blaxxun <blaxxun@blaxxun.blaxxun>
2026-09-13 18:00:41 +0200

    fixes for 1.0

--- Vitality/Properties/AssemblyInfo.cs
+++ Vitality/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Vitality")]
-[assembly: AssemblyDescription("https://valheim.thunderstore.io/package/Smoothbrain/Vitality")]
+[assembly: AssemblyDescription("https://valheim.hexium.gg/mods/Smoothbrain/Vitality")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Vitality")]
